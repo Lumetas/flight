384ea9848b0243236e58773f8297d2338ad31f70
Lumetas <98038223+Lumetas@users.noreply.github.com>
2023-11-02 16:55:08 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>flight messenger</title>
+ <link rel="stylesheet" href="style/main.css">
+ <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+</head>
+<body>
+	<header>
+		<div class='generalMessenger'>	
+			<img src="img/airplane.svg" class='airplanelogo' alt='airplane logo'>
+			<span>Flight messenger</span>
+			<img src='img/settings.svg' class='generalsettingssvg' alt='settings'>
+		</div>
+		<div class='chatHeader'>
+			<span id='nickname'>nickname</span>
+			<div>
+				<img src='img/search.svg' class='search' alt='search'>
+				<img src='img/settings.svg' class='charsettingssvg' alt='settings'>
+			</div>
+		</div>
+	</header>
+
+
+
+	<div class='content'>
+		<div class='chats'>
+
+		</div>
+
+		<div class='chatWindow'>
+			<div class='history'>
+
+			</div>
+
+			<div class='inputer'>
+				<button class='file'>📎</button>
+				<input type='text'>	
+				<button>1</button>
+				<button>2</button>
+			</div>
+		</div>
+
+
+
+	</div>
+</body>
+</html>
